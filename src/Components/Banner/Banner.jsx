f45459977193c986a5c5960fa673966a0793c3d6
ekramulislam2050 <ekramulislam2050@gmail.com>
2025-12-09 14:38:06 +0600

    added fade effect

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,23 +5,28 @@ import "../../Css/rotateBorder.css"
 
 
 const Banner = () => {
-  const typeRef = useRef(null)
-  const typedOne = useRef(false)
+  const typeRef = useRef(null);
+  const typedOne = useRef(false);
+
   useEffect(() => {
-    if (typedOne.current) return
-    typedOne.current = true
-    const text = "SSL ERP SYSTEM"
-    let i = 0
+    if (typedOne.current) return;
+    typedOne.current = true;
+
+    const text = "SSL ERP SYSTEM";
+    let i = 0;
+
     const type = () => {
       if (i < text.length) {
         typeRef.current.textContent += text[i];
         i++;
-        setTimeout(type, 70)
-      }
 
-    }
-    type()
-  }, [])
+        setTimeout(type, 70);
+      }
+    };
+
+
+    type();
+  }, []);
 
   return (
     <div className="relative w-full h-screen overflow-hidden">
@@ -41,32 +46,30 @@ const Banner = () => {
                text-transparent bg-clip-text 
                drop-shadow-[0_0_35px_rgba(0,200,255,0.6)]">
 
-          <p ref={typeRef}></p>
+          <span ref={typeRef} className=""></span>
+
         </h1>
+
+
 
         <p className="text-md md:text-xl mt-4 max-w-2xl opacity-90 leading-relaxed">
           প্রোডাকশন, ইনভেন্টরি, কোয়ালিটি ও কার্যকারিতা নিয়ন্ত্রণ করুন
 
         </p>
 
-        {/* <button className="px-6 py-2 mt-6 md:px-8 md:py-3 bg-white/20 hover:bg-white/30 rounded-xl backdrop-blur border border-white/40 
-                transition text-lg ">
-          Explore Dashboard
-        </button> */}
-
-       <button className="relative px-6 py-2 mt-6 md:px-8 md:py-3 bg-white/20 
+        <button className="relative px-6 py-2 mt-6 md:px-8 md:py-3 bg-white/20 
         hover:bg-white/30 rounded-xl backdrop-blur border border-white/40 
         transition text-lg overflow-hidden">
 
-  <span className="relative z-10">Explore Dashboard</span>
+          <span className="relative z-10">Explore Dashboard</span>
 
-  {/* Rotating Border */}
-  <span className="absolute inset-0 rounded-xl border-2 border-cyan-400 
+          {/* Rotating Border */}
+          <span className="absolute inset-0 rounded-xl border-2 border-cyan-400 
         animate-rotate-border"></span>
-</button>
+        </button>
 
 
-         
+
       </div>
     </div>
   );
