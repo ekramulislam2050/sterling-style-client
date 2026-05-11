c21019c4f35fa18b0820aee9cc38c1eb49001bb7
ekramulislam2050 <ekramulislam2050@gmail.com>
2026-05-11 10:48:59 +0600

    formate excel file

--- src/Pages/Attendance/Attendance.jsx
+++ src/Pages/Attendance/Attendance.jsx
@@ -1,45 +1,45 @@
- 
+
 import HeaderOfAttendancePage from "../../Components/HeaderOfAttendancePage/HeaderOfAttendancePage";
 import SearchAndFilterOfWorkerAttendance from "../../Components/SearchAndFilterOfWorkerAttendance/SearchAndFilterOfWorkerAttendance";
 import SummaryCardsOfAttendance from "../../Components/SummaryCardsOfAttendance/SummaryCardsOfAttendance";
 import TableOfAttendance from "../../Components/TableOfAttendance/TableOfAttendance";
 import useAxiosSecure from "../../Hooks/UseAxiosSecure/UseAxiosSecure";
-import{useQuery} from "@tanstack/react-query"
- import ErrMsg from "../../LoadingAndSuccessAndErrMsg/ErrMsg/ErrMsg"
+import { useQuery } from "@tanstack/react-query"
+import ErrMsg from "../../LoadingAndSuccessAndErrMsg/ErrMsg/ErrMsg"
 import { toast } from "react-toastify";
 import Loading from "../../LoadingAndSuccessAndErrMsg/Loading/Loading";
 
 const Attendance = () => {
-      const axiosSecure=useAxiosSecure()
-        
-      const {data,isLoading,isError}=useQuery({
-        queryKey:["attendance"],
-        queryFn:async()=>{
-            const res=await axiosSecure.get("/api/getAttendanceOfWorker")
-            return res.data
-        }
-      })
-      
-       if(isLoading){
-         return Loading("Loading please wait")
-       }
-       if(isError){
-         return ErrMsg("Something went wrong")
-       }
+  const axiosSecure = useAxiosSecure()
 
-    return (
-        <div className="pt-28 px-4 space-y-4">
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["attendance"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/api/getAttendanceOfWorker")
+      return res.data
+    }
+  })
 
-            <HeaderOfAttendancePage data={data}/>
+  if (isLoading) {
+    return Loading("Loading please wait")
+  }
+  if (isError) {
+    return ErrMsg("Something went wrong")
+  }
 
-            <SummaryCardsOfAttendance />
+  return (
+    <div className="pt-28 px-4 space-y-4">
 
-            <SearchAndFilterOfWorkerAttendance />
+      <HeaderOfAttendancePage data={data} />
 
-            <TableOfAttendance />
+      <SummaryCardsOfAttendance />
 
-        </div>
-    );
+      <SearchAndFilterOfWorkerAttendance />
+
+      <TableOfAttendance />
+
+    </div>
+  );
 };
 
 export default Attendance;
